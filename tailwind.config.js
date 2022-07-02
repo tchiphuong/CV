f6c5bef50777b1c0baa23590a1700580a3db9409
Phuong Tran <chiphuong9299@hotmail.com>
2022-07-03 00:33:27 +0700

    commit code

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: 'class',
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+//npx tailwindcss -i ./src/assets/css/styles.css -o ./src/assets/css/tailwindcss.css --watch
